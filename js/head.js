ea8f58f896d9b948d530be5896ab9e951a842fd2
DevHedronYT <altair.d.ghost@gmail.com>
2021-12-19 10:20:53 +0200

    Added JS Folder

---
+++ js/head.js
@@ -0,0 +1,4 @@
+document.write(
+	'<meta property="og:image" content="./res/hedron.jpg">\n<meta property="og:image:secure_url" content="./res/hedron.jpg">\n<meta property="og:title" content="DevHedron">\n<meta property="og:description" content="Developer">\n<meta property="og:type" content="Website">\n<meta property="og:site_name" content="DevHedron"></meta>\n<meta name="viewport" content="width=device-width, initial-scale=1">\n<link rel="icon" href="./res/hedron.jpg">\n<link rel="stylesheet" type="text/css" media="screen" href="./css/main.css"></link>'
+)
+

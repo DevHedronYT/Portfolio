a6a765390fbea55e2364468b2c6a73123941a3f1
DevHedronYT <altair.d.ghost@gmail.com>
2022-03-30 17:29:16 +0200

    Experimenting With GitHub Projects & HTML Effects

--- src/components/GithubProject.js
+++ src/components/GithubProject.js
@@ -3,11 +3,13 @@ import style from './modules/git.module.css';
 const GProj = ({name, description, stars, language, url}) => {
     return (
         <div className = {style.github_proj}>
-            <h1>{name}</h1>
-            <p>{description}</p>
-            <h1>Stars: {stars}</h1>
-            <h1>Language: {language}</h1>
-            <a href={url}>Link</a>
+            <div className = {style.container}>
+                <h1>{name}</h1>
+                <p>{description}</p>
+                <h1>Stars: {stars}</h1>
+                <h1>Using {language}</h1>
+                <a href={url}>Link</a>
+            </div>
         </div>
     );
 }
